26ad4b8e0fbf1d6bcec16c8d73737f83620b4ec3
Your Name <you@example.com>
2022-03-23 08:04:59 -0300

    anadir prototipo de funciones

--- src/main.c
+++ src/main.c
@@ -1,4 +1,103 @@
+/* Entrada y salida estándar */
 #include <stdio.h>
+
+/* manipulación de Strings */
+#include <string.h>
+
+// Tarea 1:
+/* 
+  1.- Declaración de funciones
+
+*/
+
+/*  Importar canciones desde un 
+ *  archivo CSV (char* nombre_archivo):
+ *  El usuario ingresa el nombre del
+ *  archivo y la aplicación carga 
+ *  todas las canciones.
+ */
+int importCSV(char *fileName);
+
+/*  Exportar canciones
+ *  CSV(char* nombre_archivo):
+ *  La aplicación exporta las canciones
+ *  y sus datos a un archivo csv indicado
+ *  por el usuario.
+ */
+int exportCSV(char *fileName);
+
+/* Agregar canción (char* nombre, char*
+ * artista, char** géneros, int año, char*
+ * Lista_reproducción): El usuario ingresa
+ * los datos de una canción y la aplicación
+ * deberá agregarla en la lista de reproducción
+ * indicada por el usuario. Además debe 
+ * validarse que la canción no exista
+ * previamente en la lista de reproducción. 
+ * */
+int addSong(char *name, char *artist, char **genres, int year, char *playlist);
+
+/* Buscar cancion por nombre (char* nombre):
+ * El usuario ingresa el nombre de una canción
+ * y la aplicación muestra por pantalla todas
+ * las canciones (y su respectiva información) 
+ * que tengan ese nombre. Si no existe ninguna 
+ * canción con ese nombre debe mostrar un aviso. 
+ */
+int searchSong(char *name);
+
+/*  Buscar canción por artista (char* artista):
+ *  El usuario ingresa el nombre de un artista
+ *  o banda y la aplicación muestra por pantalla
+ *  todas las canciones (y su respectiva 
+ *  información) que pertenezcan al artista 
+ *  indicado. De no existir el artista,
+ *  debe mostrar un mensaje por pantalla. 
+ */
+int searchArtist(char *artist);
+
+/* Buscar cancion por género (char* género):
+ * El usuario ingresa el nombre de un género
+ * musical y la aplicación muestra por
+ * pantalla todas las canciones (y su 
+ * respectiva información) que pertenezcan
+ * al género indicado. De no existir el
+ * género, debe mostrar un mensaje por pantalla. 
+ */
+int searchGenre(char *genre);
+
+/* Eliminar cancion (char* Nombre, char* artista, int año):
+ * El usuario ingresa el nombre de una canción y la
+ * aplicación elimina la canción correspondiente de todas
+ * las listas de reproducción correspondientes. De no 
+ * existir la canción se debe mostrar un aviso por pantalla.
+ */
+int deleteSong(char *song, char * artist, int year);
+
+/* Mostrar nombres de las listas de reproducción ():
+ * La aplicación muestra por pantalla los nombres
+ * de todas las listas de reproducción y la
+ * cantidad de canciones que tiene cada lista.
+ */
+int showPlaylists();
+
+/*  Mostrar una lista de reproducción (char* Lista_reproduccion):
+ *  El usuario ingresa el nombre de una lista de reproducción y
+ *  la aplicación muestra por pantalla todas las canciones 
+ *  pertenecientes a ella (y su respectiva información).
+ *  Si la lista de reproducción no existe se debe mostrar
+ *  un mensaje por pantalla. 
+ */
+int showPlaylist(char *playlist);
+
+/* Mostrar todas las canciones ():
+ * Muestra todas las canciones de Helen,
+ * sin importar a qué lista de
+ * reproducción pertenezca. 
+ */
+int showAllSongs();
+
+/* main function */
 int main(){
     return 0;
 }
