767251033a65a9c05029f9208ee7e5ae7a8f71f0
Your Name <you@example.com>
2022-03-27 13:07:22 -0300

    actualizar csv.c con comentarios

--- src/csv.c
+++ src/csv.c
@@ -1,30 +1,50 @@
 #include "csv.h"
 
+/* Lee el archivo csv linea por linea 
+ * y crea un struct Song por linea, 
+ * guarda respectivamente el nombre,
+ * el artista. 
+ *
+ * Para el género crea 
+ * una lista enlazada en el heap de strings (char *).
+ *
+ * SE ASUME QUE:
+ *
+ * ningun campo es mayor de 99 chars.
+ *
+ *
+ */
 void populateList(CSV *csv){
     ssize_t read; size_t length = 0; char *line = NULL;
 
+    /* leer linea a "read" hasta EOF */
     while((read = getline(&line, &length, csv->fd) != EOF)){
+        /* struct song para guardar la linea */
         Song song;
+        /* string para guardar letra x letra */
         char linecpy[100];
+        /* contador para recorrer letra x letra */
         int i = 0;
+        /* borrar la string */
         strcpy(linecpy, "");
 
         while( line[i] != ','){
-            printf("%d ", i);
             linecpy[i] = line[i];
             i++;
         }
         putchar('\n');
+
         linecpy[i] = '\0';
         strcpy(song.name, linecpy);
+
         printf("Name: %s\n", song.name);
+
 
         strcpy(linecpy, "");
 
         int j = 0;
         i++;
         while( line[i] != ','){
-            printf("%d ", i);
             linecpy[j] = line[i];
             j++;
             i++;
@@ -58,7 +78,7 @@ void populateList(CSV *csv){
 
 void importCSV(CSV *csv, char *name){
 
-    List *list = createList();
+    List *list = listCreate();
     
     csv->fd = fopen(name, "rw");
     if(!csv->fd){
