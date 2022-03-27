08fc5a3ecedbcf19a2830939224af70e38f9d349
Your Name <you@example.com>
2022-03-27 12:37:15 -0300

    renombrar funciones y arreglar structs

--- include/list.h
+++ include/list.h
@@ -3,33 +3,33 @@
 #include <stdlib.h>
 typedef struct Node {
     void *data;
-    Node *next;
-    Node *prev;
+    struct Node *next;
+    struct Node *prev;
 }Node;
 
 typedef struct LinkedList {
-    Node *currrent;
+    Node *head;
     Node *tail;
     Node *current;
 }List;
 
-Node *createNode(void * data){
+Node *nodeCreate(void * data){
 	Node *n = (Node*) calloc(1, sizeof(Node));
   	n -> data = data;
   	n -> next = NULL;
  	return n;
 }
 
-List *createList(){
-	list = (List *) calloc(1,sizeof(List));
+List *listCreate(){
+	List *list = (List *) calloc(1,sizeof(List));
 	list -> head = NULL;
 	list -> tail = NULL;
 	list -> current = NULL;
 	return list;
 }
 
-void pushBack(List * list, void * data) {
-    Node *nodo = createNode(data);
+void listPushBack(List * list, void * data) {
+    Node *nodo = nodeCreate(data);
     if(list -> head == NULL){
       list -> head = nodo;
     }else{
