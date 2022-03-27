b5079fae767aafe89497b6b338fa26cae07b283b
Your Name <you@example.com>
2022-03-27 14:19:49 -0300

    actualizar genero y crear cancion en lista

--- src/song.c
+++ src/song.c
@@ -1,10 +1,10 @@
 #include "song.h"
-Song *createSong(){
+Song *createSong(List *list){
 	Song *song = (Song *)malloc(sizeof(Song));
         strcpy(song->name, "");
         strcpy(song->artist, "");
         strcpy(song->playlist, "");
-	song->genres = NULL;
+        strcpy(song->genres, "");
 	song->year = 0;
 	return song;
 }
