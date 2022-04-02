2cf450b112f90d4807a102b4502c99596f31b65f
Your Name <you@example.com>
2022-04-02 01:42:24 -0300

    arreglar busquedas

--- src/search.c
+++ src/search.c
@@ -31,16 +31,46 @@ int searchGenre(List *list){
     printf("Introduce el genero que desea buscar:\n");
     scanf("%[^\n]*s", busqueda);
     getchar();
-    for(Song *song = listFirst(list); song != NULL;song = listNext(list)) {
-        for(char* name = listHead(song->genres); name != NULL; name = listNext(list)) {
+    for(Song *song = listFirst(list);
+            song != NULL;
+            song = listNext(list)) {
+        for(char* name = listFirst(song->genres); 
+                name != NULL;
+                name = listNext(song->genres)) {
+
+
             if (strcmp(busqueda, name) == 0) {
                 mostrarCancion(song);
+                found++;
+                break;
             }
         }
 
     }
     if (!found) {
         strcat(buf, "\033[0;31mError: Ninguna cancion con ese genero en la lista \033[0m \n");
+    }
+    return EXIT_SUCCESS;
+}
+
+int searchArtist(List *list) {
+    char busqueda[30];
+    int found = 0;
+
+    printf("Introduce del artista de la canción a buscar:\n");
+    scanf("%[^\n]*s", busqueda);
+    getchar();
+
+    for (Song *song = listFirst(list); song != NULL; song = listNext(list)){
+        if(strcmp(song->artist, busqueda) == 0){
+            mostrarCancion(song);
+            found++;
+        }
+    }
+    if (!found) {
+        strcat(buf, "\033[0;31mError: Cancion no encontrada (");
+        strcat(buf, busqueda);
+        strcat(buf, ")\033[0m\n");
     }
     return EXIT_SUCCESS;
 }
