d9873d19384cd124819abcce94e13acad22d6302
Your Name <you@example.com>
2022-04-02 00:04:35 -0300

    mover funciones a sus respectivos headers.

--- include/song.h
+++ include/song.h
@@ -1,6 +1,7 @@
 #ifndef SONG_H
 #define SONG_H
 #include "list.h"
+#include "util.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -13,5 +14,26 @@ typedef struct {
 }Song;
 
 Song *createSong();
+
+
+/* Agregar canción (char* nombre, char*
+ * artista, char** géneros, int año, char*
+ * Lista_reproducción): El usuario ingresa
+ * los datos de una canción y la aplicación
+ * deberá agregarla en la lista de reproducción
+ * indicada por el usuario. Además debe 
+ * validarse que la canción no exista
+ * previamente en la lista de reproducción. 
+ * */
+int addSong();
+
+
+/* Eliminar cancion (char* Nombre, char* artista, int año):
+ * El usuario ingresa el nombre de una canción y la
+ * aplicación elimina la canción correspondiente de todas
+ * las listas de reproducción correspondientes. De no 
+ * existir la canción se debe mostrar un aviso por pantalla.
+ */
+int deleteSong(List *list);
 
 #endif
