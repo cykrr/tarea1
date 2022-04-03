3f55b10e67b17ddcc7cf274c67c8d57f5ab0cab2
Your Name <you@example.com>
2022-04-03 10:00:10 -0400

    anadir color roko

--- include/util.h
+++ include/util.h
@@ -1,6 +1,11 @@
 #ifndef UTIL_H
 #define UTIL_H
 #include "song.h"
+
+#define COLOR_RESET   "\033[0m"
+#define COLOR_RED "\033[31m"      /* Red */
+
 extern char buf[];
 List * genresToList(const char * genre);
+
 #endif
