b173f96a283eb3545475a39d440a0fcfb59ed6ab
Your Name <you@example.com>
2022-04-13 01:23:35 -0400

    renombrar lista

--- include/carrito.h
+++ include/carrito.h
@@ -1,9 +1,10 @@
 #ifndef CARRITO_H
 #define CARRITO_H
 #include <string.h>
+#include "list.h"
 typedef struct Carrito {
     char *nombre;
     // Necesitamos implementar una pila en vez de una lista
-    Lista *lista;
+    List *lista;
 } Carrito;
 #endif
