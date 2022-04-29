c4b1e13919817340883a49fb56f1fb5c90a106be
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2022-04-28 22:49:36 -0400

    renombrar carrito para coincidir con estetica del programa
    s

--- include/carrito.h
+++ include/carrito.h
@@ -2,12 +2,19 @@
 #define CARRITO_H
 #include <string.h>
 #include "list.h"
-typedef struct Carrito {
-    char *nombre;
+#include "map.h"
+typedef struct Cart {
+    char *name;
     // Necesitamos implementar una pila en vez de una lista
-    List *lista;
+    List *list;
+
+    int size;
 } Cart;
 
 int deleteItem(Cart *cart);
+
+void addToCart(Map *mapCarts);
+
+Cart *cartCreate(char *cartName);
 
 #endif
