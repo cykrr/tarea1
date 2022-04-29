7c35367a01ab4818cde1a59c34d9862e7427718c
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2022-04-28 23:10:53 -0400

    definicion funciones añadir al carrito y mostrar carritos

--- src/carrito.c
+++ src/carrito.c
@@ -21,3 +21,48 @@ Cart *cartCreate(char *cartName) {
     cart->list = listCreate();
     return cart;
 }
+
+void addToCart(Map *mapCarts, Map* mapName) {
+    char cartName[60];
+    char productName[100];
+    printf("Ingrese el nombre del carrito: ");
+    scanf("%[^\n]*s", cartName);
+    getchar();
+    Cart *cart = searchMap(mapCarts, cartName);
+    if (!cart) {
+        cart = cartCreate(cartName);
+    }
+    printf("Ingrese nombre del producto a agregar");
+    scanf("%[^\n]*s", productName);
+
+    Item *item = searchMap(mapName, productName);
+
+    // nombre producto
+    // cuantos productos?
+    // añadir producto.
+}
+
+void showCarts(Map *mapCarts) {
+    for(Cart *cart = firstMap(mapCarts); 
+            cart != NULL; 
+            cart = nextMap(mapCarts)) {
+        strcat(buf, "Nombre carrito: ");
+        strcat(buf, cart->name);
+        strcat(buf, "\n");
+        strcat(buf, "Cantidad de productos: ");
+        {
+            char productos[10];
+            sprintf(productos, "%d", cart->size);
+            strcat(buf, productos);
+        }
+        strcat(buf, "\n");
+        strcat(buf, "Total a pagar: ");
+        {
+            char total[10];
+            sprintf(total, "%d", cart->total);
+            strcat(buf, total);
+        }
+        strcat(buf, "\n");
+
+    }
+}
