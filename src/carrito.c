76d546d64e16c65d35733d087d9d1bc8ee043ee5
Matias <matias.romero.a@mail.pucv.cl>
2022-04-30 16:01:08 -0400

    Mensaje de popLastCart arreglado

--- src/carrito.c
+++ src/carrito.c
@@ -173,7 +173,7 @@ void popLastCart(Map* mapCarts, Map* mapNames){
     getchar();
     Cart* cartAux = searchMap(mapCarts, cartName);
     CartItem* item = listLast(cartAux->list);
-    
+
     if (!cartAux)
     {
         strcat(buf, "El carrito \"");
@@ -181,9 +181,9 @@ void popLastCart(Map* mapCarts, Map* mapNames){
         strcat(buf, "\" no existe\n");
     }else{
 
-        strcat(buf, "El producto: ");
+        strcat(buf, "El producto ");
         strcat(buf, item->item->name);
-        strcat(buf, " fue eliminado");
+        strcat(buf, " fue eliminado\n\n");
 
         listPopBack(cartAux->list);
         item->stock -= 1;
